cad99b333670f8b2a1bbc090bc40328142b5b50c
feiaunicamp <51330717+feiaunicamp@users.noreply.github.com>
2019-09-19 18:46:25 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 	///////////////////////////
 	// Preloader
 	$(window).on('load', function() {
-		$("#preloader").delay(600).fadeOut();
+		$("#preloader").delay(100).fadeOut();
 	});
 
 	///////////////////////////
@@ -91,3 +91,37 @@
 	});
 
 })(jQuery);
+
+
+
+var acc = document.getElementsByClassName("cardnome");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
